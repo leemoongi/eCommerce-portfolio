88e88a49c51435e15164cce3b4c39d07242ca389
LeeMoongi <01ki07@starlas.co.kr>
2022-12-27 17:45:38 +0900

    처음 커밋

---
+++ src/main/java/com/home/eCommerceportfolio/ECommercePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.home.eCommerceportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/eCommerceportfolio/ECommercePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.eCommerceportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
